f36c5952473e71650ffd8f59fae2933c1c65d20d
shimauchi_haruki <tontonkirikiri@gmail.com>
2021-07-25 11:48:52 +0900

    warning!!! the program does not finished!!! ahhhhhhhhhhhhhhhhhhhhhhh!!!!!!!!!! why...

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 2 methods
 
 first,appear question.
-second,notice to miss.
+second,notice to miss.  #  
 
 first method is repeated 'while'.
 """
+miss=0
+import random as rd
 
-def appearQuestion:
-    
+def appearQuestion():
+    number1=rd.randint(0,10)
+    number2=rd.randint(0,10)
 
-def notice:
-    
+    message=number1, "+", number2, "=?"
+    print(number1, "+", number2, "=?")
+    answer=int(input()) #  i want to fix this int to be able to float.
+    if(answer == number1+number2):
+        input("correct!!")
+    else:
+        miss=1
+        print("incorrect...")
+        
+        print(miss)
+
 
 while miss == 0:
     appearQuestion() 
-
-notice()
+    print(miss)
 
 input("Press return...")
