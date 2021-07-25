dfc203a062cec6d94df99c48c445e3bb2a2b7d88
shimauchi_haruki <tontonkirikiri@gmail.com>
2021-07-25 10:59:52 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+2 methods
+
+first,appear question.
+second,notice to miss.
+
+first method is repeated 'while'.
+"""
+
+def appearQuestion:
+    
+
+def notice:
+    
+
+while miss == 0:
+    appearQuestion() 
+
+notice()
+
+input("Press return...")
